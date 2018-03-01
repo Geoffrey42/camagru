2f24ac1da3d9c09091163d7dce58d8f47cee5cc9
Geoffrey42 <geoffreygane@gmail.com>
2018-03-01 20:20:50 +0100

    add some html in index.php to test if docker configuration is ok

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Hello Universe!</title>
+    </head>
+    <body>
+        <img src="http://tech.osteel.me/images/2015/12/18/docker-tutorial2.gif" alt="Hello World!" />
+    </body>
+</html>
